d3610518ed6e7f1c8d1444208b0111826f41198f
Alex Vovchuk <olexvov@gmail.com>
2025-01-10 17:52:51 -0700

    update messages count

--- src/utils/chatParser.js
+++ src/utils/chatParser.js
@@ -1,18 +1,24 @@
 export function parseLinkedInChat() {
-  const messages = document.querySelectorAll(
+  const parsedMessages = document.querySelectorAll(
     ".msg-s-event-listitem__message-bubble--msg-fwd-enabled"
   );
 
-  const markedMessages = Array.from(messages)
-    .map((message) => {
-      return {
-        text: message?.textContent?.trim(), // The message element itself
-        owner: message.closest(".msg-s-event-listitem--other")
-          ? "recruiter"
-          : "me", // Check for any ancestor with the selector
-      };
-    })
-    .map((message) => `${message.owner}: ${message.text}`);
+  const markedMessages = Array.from(parsedMessages).map((message) => {
+    return {
+      text: message?.textContent?.trim(), // The message element itself
+      author: message.closest(".msg-s-event-listitem--other")
+        ? "recruiter"
+        : "me", // Check for any ancestor with the selector
+    };
+  });
 
-  return markedMessages;
+  const messages = markedMessages.map(
+    (message) => `${message.author}: ${message.text}`
+  );
+
+  const recruiterMessageCount = [...markedMessages].filter(
+    (message) => message.author === "recruiter"
+  ).length;
+
+  return { messages, recruiterMessageCount };
 }
